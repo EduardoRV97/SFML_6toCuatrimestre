b1f8d717cfd5cc3cf59f19af2dfd85ff767c04ee
EduardoRV97 <erivera39414@ucq.edu.mx>
2024-09-03 08:53:56 -0600

    Clase 2 - Grafica computacionales
    Se crearon la clase BaseApp donde se movieron los metodos, para permitir un mejor encapsulamiento.
    Se redujeron la lineas de main a un minimo.

---
+++ SFML_6toCuatrimestre/SFML_6toCuatrimestre/Source/BaseApp.cpp
@@ -0,0 +1,59 @@
+#include "BaseApp.h"
+
+//Funcion que corre el programa en main - Declaracion
+
+void BaseApp::run() {
+    initialize();
+    while (window->isOpen())
+    {
+        handleEvents();
+        update();
+        render();
+    }
+    cleanup();
+}
+
+// Funcion de inicializacion - Declaracion
+
+void BaseApp::initialize() {
+    window = new sf::RenderWindow(sf::VideoMode(800, 600), "YIYI Engine");
+    shape = new sf::RectangleShape(sf::Vector2f(200, 200));
+    shape->setFillColor(sf::Color::Green);
+    shape2 = new sf::CircleShape(300, 300);
+    shape2->setFillColor(sf::Color::Cyan);
+    shape2->setOutlineThickness(20);
+    shape2->setOutlineColor(sf::Color::Red);
+}
+
+// Funcion de manejo de datos - Declaracion
+
+void BaseApp::handleEvents() {
+    sf::Event event;
+    while (window->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window->close();
+    }
+}
+
+//Funcion que se actualiza por frame - Declaracion
+
+void BaseApp::update() {
+
+}
+
+//Funcion de renderizado - Declaracion
+
+void BaseApp::render() {
+    window->clear();
+    window->draw(*shape);
+    window->draw(*shape2);
+    window->display();
+}
+
+//Funcion de liberacion de memoria - Declaracion
+
+void BaseApp::cleanup() {
+    delete window;
+    delete shape;
+}
---
+++ SFML_6toCuatrimestre/SFML_6toCuatrimestre/include/BaseApp.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class
+	BaseApp {
+public:
+	BaseApp() = default;
+	~BaseApp() = default;
+
+    // Funcion que corre el programa en main
+    void
+        run();
+
+    // Funcion de inicializacion
+    void
+        initialize();
+
+    // Funcion de manejo de datos
+    void
+        handleEvents();
+
+    // Funcion que se actualiza por frame
+    void
+        update();
+
+    // Funcion de renderizado
+    void
+        render();
+
+    //Funcion de liberacion de memoria
+    void
+        cleanup();
+
+private:
+
+    //Variables Globales
+	sf::RenderWindow* window;
+	sf::RectangleShape* shape;
+    sf::CircleShape* shape2;
+};
