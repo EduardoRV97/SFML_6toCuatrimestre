7cbc6c948f0c04a96e886aa03cb18042423fa9ac
EduardoRV97 <erivera39414@ucq.edu.mx>
2024-10-11 18:48:16 -0600

    Cambios 11/10
    Cambios de clases

--- SFML_6toCuatrimestre/SFML_6toCuatrimestre/Source/Actor.cpp
+++ SFML_6toCuatrimestre/SFML_6toCuatrimestre/Source/Actor.cpp
@@ -9,6 +9,7 @@ Actor::Actor(std::string actorName)
 	// Setup Shape
 	EngineUtilities::TSharedPointer<ShapeFactory> shape = EngineUtilities::MakeShared<ShapeFactory>();
 	addComponent(shape);
+
 	// Setup Transform
 	
 	// Setup Sprite
@@ -21,7 +22,7 @@ void Actor::update(float deltaTime) {
 }
 
 void Actor::render(Window& window) {
-	for (unsigned int i = 0; i < components.size(); i++)
+  for (unsigned int i = 0; i < components.size(); i++)
 	{
 		window.draw(*components[i].dynamic_pointer_cast<ShapeFactory>()->getShape());
 	}
